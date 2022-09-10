d8efeb0a409eb1e5dec34a15f837cef1a497ea20
Fabiola-Suarez <fabiolaelena98@gmail.com>
2022-09-10 12:21:21 -0300

    Error encontrado en entidad

--- java/src/model/MainBatallaZapatoGallotronix.java
+++ java/src/model/MainBatallaZapatoGallotronix.java
@@ -2,7 +2,7 @@ import model.batalla.Batalla;
 import model.chimpos.Gallotronix;
 import model.chimpos.Zapato;
 
-public class MainBatallaZapatoGallotronix extends Referi {
+public class MainBatallaZapatoGallotronix {
 
     public static void main(String[] args) {
         Zapato zapato = new Zapato("Zapato", 5);
