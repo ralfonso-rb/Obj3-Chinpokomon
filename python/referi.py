6adc13acf2b51f3cb840572b1bb0681701084b31
Fabiola-Suarez <fabiolaelena98@gmail.com>
2022-09-16 20:48:18 -0300

    creando referi en python

---
+++ python/referi.py
@@ -0,0 +1,16 @@
+from batalla import *
+
+class Referi ():
+    def __init__(self, batalla):
+        self._batalla = batalla
+    
+    @property
+    def batalla(self):
+        return self._batalla
+
+    @batalla.setter
+    def batalla(self, batalla):
+        self._batalla = batalla
+    
+    def gestionar_batalla(self):
+        self.batalla.comenzar_batalla()
