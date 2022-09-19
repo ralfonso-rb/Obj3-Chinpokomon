b1ec0354b28a05d83abbdd30b6d00f64615b14e2
FrancoMostafa <francom2342@gmail.com>
2022-09-19 19:23:04 -0300

    chimpo abstracto

--- java/src/model/chimpos/Chinpokomon.java
+++ java/src/model/chimpos/Chinpokomon.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Chinpokomon {
+public abstract class Chinpokomon {
     String nombre;
     Integer nivel;
     Integer vida;
